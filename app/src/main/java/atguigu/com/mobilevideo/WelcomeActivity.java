e6c0cfe0daddba20a25c972e1730c185609cd7fe
liyinqing <yinqing_521@126.com>
2017-05-21 10:59:10 +0800

    Initial commit

---
+++ app/src/main/java/atguigu/com/mobilevideo/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package atguigu.com.mobilevideo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainPaper();
+            }
+        },2000);
+    }
+    private void startMainPaper() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainPaper();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+}
